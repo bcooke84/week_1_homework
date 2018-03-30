3acb4a45fa359a17009ec77aeefdb040b3655af8
Brian Cooke <brian.cooke@live.co.uk>
2018-03-30 14:58:03 +0100

    10 assertions

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -6,10 +6,32 @@ def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(pet_shop, num)
-  return pet_shop[:admin][:total_cash] += num
+def add_or_remove_cash(pet_shop, cash)
+  return pet_shop[:admin][:total_cash] += cash
 end
 
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, num)
+  pet_shop[:admin][:pets_sold] = num
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].size()
+end
+
+def pets_by_breed(pet_shop, breed)
+  breed_matches = []
+  for each_pet in pet_shop[:pets]
+    breed_matches.push(each_pet) if each_pet[:breed] == breed
+  end
+  return breed_matches
+end
+
+def find_pet_by_name(pet_shop, name)
+  for each_pet in pet_shop[:pets]
+    return each_pet if each_pet[:name] == name
+  end
 end
